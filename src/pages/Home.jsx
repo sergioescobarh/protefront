51fc5fa84363c10beac8c252a4b78752ca02e91d
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-04-21 01:06:42 -0500

    first structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import './home.scss'
+import Header from '../organisms/Header';
+import React from 'react'
+
+
+export default function Home() {
+    return (
+    <div className='home'>
+        <header>  <Header/>  </header>
+        <main>
+            main
+        </main>
+    </div>
+    )
+}
